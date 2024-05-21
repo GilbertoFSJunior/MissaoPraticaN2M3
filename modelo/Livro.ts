1b9349cddafc3326d2b72b8bd557f624cc022278
GilbertoFSJunior <142622373+GilbertoFSJunior@users.noreply.github.com>
2024-05-21 00:36:32 -0300

    Delete modelo directory

--- modelo/Livro.ts
+++
@@ -1,9 +0,0 @@
-interface Livro {
-  codigo: number;
-  titulo: string;
-  resumo: string;
-  autores: string[]; // Ou o tipo apropriado para os autores
-  codEditora: number; // Ou o tipo apropriado para a editora
-}
-
-export default Livro;
